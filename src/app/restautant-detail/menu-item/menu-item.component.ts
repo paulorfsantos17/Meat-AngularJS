87cc6ac87fbfc89c98af7184d981693bb9853dab
paulorfsantos17 <paulorfsantos17@gmail.com>
2018-08-13 14:16:51 -0300

    animations keyframes

--- src/app/restautant-detail/menu-item/menu-item.component.ts
+++ src/app/restautant-detail/menu-item/menu-item.component.ts
@@ -1,10 +1,23 @@
 import { Component, OnInit , Input, EventEmitter, Output } from '@angular/core';
 import {MenuItem} from './menu-item.model'
+import {trigger,state, style, transition, animate} from "@angular/animations"
+
 @Component({
   selector: 'mt-menu-item',
-  templateUrl: './menu-item.component.html'
+  templateUrl: './menu-item.component.html',
+   animations:[
+  trigger('menuItemAppeared',[
+   	state('ready', style({opacity:1})),
+   	transition('void => ready', [
+   		 style({opacity:0, transform: "translateY(-20px)"}),
+   		 animate( '1000ms 0ms ease-in')
+   		])
+
+   	])]
+
 })
 export class MenuItemComponent implements OnInit {
+	menuState="ready"
 
 @Input() 	menuItem: MenuItem
 @Output() add = new EventEmitter()
--- src/app/restautant-detail/shopping-card/shopping-card.component.ts
+++ src/app/restautant-detail/shopping-card/shopping-card.component.ts
@@ -1,12 +1,30 @@
 import { Component, OnInit } from '@angular/core';
 import{ShoppingCardService} from './shopping-card.service'
+import {trigger,state, style, transition, animate, keyframes} from "@angular/animations"
 
 @Component({
   selector: 'mt-shopping-card',
-  templateUrl: './shopping-card.component.html'
+  templateUrl: './shopping-card.component.html',
+  animations: [
+    trigger('row',[
+      state('ready',style({opacity:1})),
+      transition("void => ready", animate ('300ms 0ms ease-in', keyframes([
+        style({opacity:0 , transform:"translateX(-50px)", offset:0}),
+        style({opacity:0.8 , transform:"translateX(10px)", offset:0.8}),
+        style({opacity:1 , transform:"translateX(0px)", offset:1})
+        ]))),
+       transition("ready => void", animate ('300ms 0ms ease-out', keyframes([
+        style({opacity:1 , transform:"translateX( 0px)", offset:0}),
+        style({opacity:0.8 , transform:"translateX(10px)", offset:0.2}),
+        style({opacity:0 , transform:"translateX(-30px)", offset:1})
+        ])))
+      ])
+  ]
 })
   
 export class ShoppingCardComponent implements OnInit {
+
+  rowState="ready"
 
   constructor( private shoppingCardService: ShoppingCardService) { }
 
